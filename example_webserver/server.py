d7b68bff938d2895f5e8caad09ed7d420af88c81
disal <disal@admin.git>
2025-03-04 16:34:08 +0000

    update server with  ngrok and use_microphone False

--- example_webserver/server.py
+++ example_webserver/server.py
@@ -4,14 +4,16 @@ if __name__ == '__main__':
     server = "0.0.0.0"
     port = 5025
 
-    print (f"STT speech to text server")
-    print (f"runs on http://{server}:{port}")
-    print ()
-    print ("starting")
-    print ("└─ ... ", end='', flush=True)
+    print(f"STT speech to text server")
+    print(f"runs on http://{server}:{port}")
+    print()
+    print("starting")
+    print("└─ ... ", end='', flush=True)
 
     from RealtimeSTT import AudioToTextRecorder
     from colorama import Fore, Style
+    from pyngrok import ngrok
+    import nest_asyncio
     import websockets
     import threading
     import colorama
@@ -27,20 +29,20 @@ if __name__ == '__main__':
     first_chunk = True
     full_sentences = []
     displayed_text = ""
-    message_queue = queue.Queue() 
+    message_queue = queue.Queue()
     start_recording_event = threading.Event()
     start_transcription_event = threading.Event()
     connected_clients = set()
+    audio_chunks = queue.Queue()
 
     def clear_console():
         os.system('clear' if os.name == 'posix' else 'cls')
 
     async def handler(websocket, path):
-
-        print ("\r└─ OK")
+        print("\r└─ OK")
         if WAIT_FOR_START_COMMAND:
             print("waiting for start command")
-            print ("└─ ... ", end='', flush=True)
+            print("└─ ... ", end='', flush=True)
 
         connected_clients.add(websocket)
 
@@ -49,29 +51,30 @@ if __name__ == '__main__':
                 async for message in websocket:
                     data = json.loads(message)
                     if data.get("type") == "command" and data.get("content") == "start-recording":
-                        print ("\r└─ OK")
-                        start_recording_event.set() 
+                        print("\r└─ OK")
+                        start_recording_event.set()
+                    elif data.get("type") == "audio":
+                        audio_chunk = data.get("content")
+                        audio_chunks.put(audio_chunk)
 
         except json.JSONDecodeError:
-            print (Fore.RED + "STT Received an invalid JSON message." + Style.RESET_ALL)
+            print(Fore.RED + "STT Received an invalid JSON message." + Style.RESET_ALL)
         except websockets.ConnectionClosedError:
-            print (Fore.RED + "connection closed unexpectedly by the client" + Style.RESET_ALL)
+            print(Fore.RED + "connection closed unexpectedly by the client" + Style.RESET_ALL)
         except websockets.exceptions.ConnectionClosedOK:
             print("connection closed.")
         finally:
-
             print("client disconnected")
             connected_clients.remove(websocket)
-            print ("waiting for clients")
-            print ("└─ ... ", end='', flush=True)
-
+            print("waiting for clients")
+            print("└─ ... ", end='', flush=True)
 
     def add_message_to_queue(type: str, content):
         message = {
             "type": type,
             "content": content
         }
-        message_queue.put(message)    
+        message_queue.put(message)
 
     def fill_cli_line(text):
         columns, _ = shutil.get_terminal_size()
@@ -87,9 +90,8 @@ if __name__ == '__main__':
 
             message = fill_cli_line(text)
 
-            message ="└─ " + Fore.CYAN + message[:-3] + Style.RESET_ALL
+            message = "└─ " + Fore.CYAN + message[:-3] + Style.RESET_ALL
             print(f"\r{message}", end='', flush=True)
-
 
     async def broadcast(message_obj):
         if connected_clients:
@@ -127,12 +129,13 @@ if __name__ == '__main__':
         'min_gap_between_recordings': 0,
         'enable_realtime_transcription': True,
         'realtime_processing_pause': 0,
+        'use_microphone': False,
         'realtime_model_type': 'tiny.en',
         'on_realtime_transcription_stabilized': text_detected,
-        'on_recording_start' : recording_started,
-        'on_vad_detect_start' : vad_detect_started,
-        'on_wakeword_detection_start' : wakeword_detect_started,
-        'on_transcription_start' : transcription_started,
+        'on_recording_start': recording_started,
+        'on_vad_detect_start': vad_detect_started,
+        'on_wakeword_detection_start': wakeword_detect_started,
+        'on_transcription_start': transcription_started,
     }
 
     recorder = AudioToTextRecorder(**recorder_config)
@@ -142,39 +145,30 @@ if __name__ == '__main__':
             start_transcription_event.wait()
             text = "└─ transcribing ... "
             text = fill_cli_line(text)
-            print (f"\r{text}", end='', flush=True)
-            sentence = recorder.transcribe()
-            print (Style.RESET_ALL + "\r└─ " + Fore.YELLOW + sentence + Style.RESET_ALL)
+            print(f"\r{text}", end='', flush=True)
+            while not audio_chunks.empty():
+                chunk = audio_chunks.get()
+                recorder.feed_audio(chunk)
+            sentence = recorder.transcript_process()
+            print(Style.RESET_ALL + "\r└─ " + Fore.YELLOW + sentence + Style.RESET_ALL)
             add_message_to_queue("full", sentence)
             start_transcription_event.clear()
             if WAIT_FOR_START_COMMAND:
                 print("waiting for start command")
-                print ("└─ ... ", end='', flush=True)
+                print("└─ ... ", end='', flush=True)
 
-    def recorder_thread():
-        global first_chunk
-        while True:
-            if not len(connected_clients) > 0:
-                time.sleep(0.1)
-                continue
-            first_chunk = True
-            if WAIT_FOR_START_COMMAND:
-                start_recording_event.wait() 
-            print("waiting for sentence")
-            print ("└─ ... ", end='', flush=True)
-            recorder.wait_audio()
-            start_transcription_event.set()
-            start_recording_event.clear()
-
-    threading.Thread(target=recorder_thread, daemon=True).start()
     threading.Thread(target=transcriber_thread, daemon=True).start()
+
+    ngrok_tunnel = ngrok.connect(port)
+    print('Public URL:', ngrok_tunnel.public_url)
+    nest_asyncio.apply()
 
     start_server = websockets.serve(handler, server, port)
     loop = asyncio.get_event_loop()
 
-    print ("\r└─ OK")
-    print ("waiting for clients")
-    print ("└─ ... ", end='', flush=True)
+    print("\r└─ OK")
+    print("waiting for clients")
+    print("└─ ... ", end='', flush=True)
 
     loop.run_until_complete(start_server)
     loop.create_task(send_handler())
