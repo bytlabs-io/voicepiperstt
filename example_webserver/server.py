c726df5ab594712b011564768a443896549c6962
disal <disal@admin.git>
2025-03-05 00:15:37 +0000

    testing

--- example_webserver/server.py
+++ example_webserver/server.py
@@ -149,8 +149,6 @@ if __name__ == '__main__':
         connected_clients.add(websocket) 
         try:
             while True:
-                async for message in websocket:
-                    print(message)
                 data = await websocket.receive_json()
                 if data.get("type") == "command" and data.get("content") == "start-recording":
                     print("\r└─ OK")
