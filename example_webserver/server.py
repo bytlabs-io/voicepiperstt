7192f254f554aa4a7df2f922f77427c78030100f
disal <disal@admin.git>
2025-03-05 02:09:32 +0000

    testing

--- example_webserver/server.py
+++ example_webserver/server.py
@@ -4,7 +4,7 @@ import logging
 
 
 WAIT_FOR_START_COMMAND = False
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
 if __name__ == '__main__':
     server = "0.0.0.0"
